7a2e04a39cf5bedb28f9f8507e0e3011e9ac22bb
OldRabbit736 <sylvan0212@gmail.com>
2022-06-03 13:10:28 +0900

    Complete lecture 19 (Throws and Timeout)

---
+++ src/test/java/org/example/DemoUtilsTest.java
@@ -0,0 +1,119 @@
+package org.example;
+
+import org.junit.jupiter.api.*;
+
+import java.time.Duration;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+//@DisplayNameGeneration(DisplayNameGenerator.Simple.class)
+class DemoUtilsTest {
+
+    DemoUtils demoUtils;
+
+    @BeforeEach
+    void setupBeforeEach() {
+        demoUtils = new DemoUtils();
+    }
+
+    @Test
+    @DisplayName("Equals and Not Equals")
+    void testEqualsAndNotEquals() {
+
+        assertEquals(6, demoUtils.add(2, 4), "2 + 4 must be 6");
+        assertNotEquals(6, demoUtils.add(1, 9), "1 + 9 must not be 6");
+    }
+
+    @Test
+    @DisplayName("Null and Not Null")
+    void testNullAndNotNull() {
+
+        assertNull(demoUtils.checkNull(null), "Object should be null");
+        assertNotNull(demoUtils.checkNull("null"), "Object should be null");
+    }
+
+    @Test
+    @DisplayName("Same and Not Same")
+    void testSameAndNotSame() {
+
+        String str = "luv2code";
+
+        assertSame(demoUtils.getAcademy(), demoUtils.getAcademyDuplicate(), "Objects should refer to the same object.");
+        assertNotSame(str, demoUtils.getAcademy(), "Objects should not refer to same object");
+    }
+
+    @Test
+    @DisplayName("True and False")
+    void testTrueFalse() {
+
+        int gradeOne = 10;
+        int gradeTwo = 5;
+
+        assertTrue(demoUtils.isGreater(gradeOne, gradeTwo), "This should return true");
+        assertFalse(demoUtils.isGreater(gradeTwo, gradeOne), "This should return false");
+    }
+
+    @Test
+    @DisplayName("Array Equals")
+    void testArrayEquals() {
+        String[] stringArray = {"A", "B", "C"};
+
+        assertArrayEquals(stringArray, demoUtils.getFirstThreeLettersOfAlphabet(), "Arrays should be equal");
+    }
+
+    @Test
+    @DisplayName("Iterable Equals")
+    void testIterableEquals() {
+        List<String> theList = List.of("luv", "2", "code");
+
+        assertIterableEquals(theList, demoUtils.getAcademyInList(), "Expected list should be equal to the actual list");
+        assertNotSame(theList, demoUtils.getAcademyInList());
+    }
+
+    @Test
+    @DisplayName("Lines Match")
+    void testLinesMatch() {
+        // expected
+        List<String> theList = List.of("luv", "2", "code");
+
+        assertLinesMatch(theList, demoUtils.getAcademyInList(), "Lines should match");
+    }
+
+    @Test
+    @DisplayName("Throws and Does Not Throw")
+    void testThrowsAndDoesNotThrow() {
+        assertThrows(Exception.class, () -> {
+            demoUtils.throwException(-1);
+        }, "Should throw exception");
+
+        assertDoesNotThrow(() -> {
+            demoUtils.throwException(0);
+        }, "Should not throw exception");
+    }
+
+    @Test
+    @DisplayName("Timeout")
+    void testTimeout() {
+        assertTimeoutPreemptively(Duration.ofSeconds(3), () -> {
+            demoUtils.checkTimeout();
+        }, "Method should execute in 3 seconds");
+    }
+
+//    @AfterEach
+//    void tearDownAfterEach() {
+//        System.out.println("Running @AfterEach");
+//        System.out.println();
+//    }
+//
+//    @BeforeAll
+//    static void setupBeforeAll() {
+//        System.out.println("@BeforeAll executes only once before all test methods in the class");
+//        System.out.println();
+//    }
+//
+//    @AfterAll
+//    static void tearDownAfterAll() {
+//        System.out.println("@AfterAll executes only once after all test methods in the class");
+//    }
+}
