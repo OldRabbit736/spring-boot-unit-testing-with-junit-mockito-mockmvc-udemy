6ecfe5606a8aa91fb932dcaf78d042b4ba3c742c
OldRabbit736 <sylvan0212@gmail.com>
2022-07-21 16:11:39 +0900

    lecture 39 complete

--- src/main/java/org/example/tdd/FizzBuzz.java
+++ src/main/java/org/example/tdd/FizzBuzz.java
@@ -7,6 +7,20 @@ public class FizzBuzz {
     // If number is divisible by 3 and 5, print FizzBuzz
     // If number is NOT divisible by 3 or 5, then print the number
 
+
+    // second implementation
+    public static String compute(int i) {
+        StringBuilder result = new StringBuilder();
+
+        if (i % 3 == 0) result.append("Fizz");
+        if (i % 5 == 0) result.append("Buzz");
+        if (result.isEmpty()) result.append(i);
+
+        return result.toString();
+    }
+
+    // first implementation
+    /*
     public static String compute(int i) {
         if (i % 3 == 0 && i % 5 == 0) return "FizzBuzz";
 
@@ -15,5 +29,5 @@ public class FizzBuzz {
         if (i % 5 == 0) return "Buzz";
 
         return String.valueOf(i);
-    }
+    }*/
 }
