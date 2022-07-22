49af7dd1d5302288e9c937a1c57f68428f36df7a
OldRabbit736 <sylvan0212@gmail.com>
2022-07-22 10:07:33 +0900

    complete lecture 50 - Spring Boot Unit Testing - Prototype Beans

--- spring-boot-unit-testing/src/test/java/com/luv2code/test/ApplicationExampleTest.java
+++ spring-boot-unit-testing/src/test/java/com/luv2code/test/ApplicationExampleTest.java
@@ -9,12 +9,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 // main과 test의 패키지 명이 다를 때 SpringBootApplication 클래스를 얄려줘야 한다.
 @SpringBootTest(classes = MvcTestingExampleApplication.class)
@@ -36,16 +36,21 @@ public class ApplicationExampleTest {
     private String schoolName;
 
     @Autowired
-    CollegeStudent student;
+    CollegeStudent student; // prototype bean
 
     @Autowired
     StudentGrades studentGrades;
+
+    @Autowired
+    ApplicationContext context;
 
     @BeforeEach
     public void beforeEach() {
         count = count + 1;
         System.out.println("Testing: " + appInfo + " which is " + appDescription +
                 "  Version: " + appVersion + ". Execution of test method " + count);
+
+        // student 는 prototype bean 이기 때문에 각 테스트가 시작될 때마다 새로운 객체로 교환되는 듯 하다.
         student.setFirstname("Eric");
         student.setLastname("Roby");
         student.setEmailAddress("eric.roby@luv2code_school.com");
@@ -68,4 +73,61 @@ public class ApplicationExampleTest {
                 student.getStudentGrades().getMathGradeResults()
         ));
     }
+
+    @DisplayName("Is grade greater")
+    @Test
+    public void isGradeGreaterStudentGrades() {
+        assertTrue(studentGrades.isGradeGreater(90, 75),
+                "failure - should be true");
+    }
+
+    @DisplayName("Is grade greater false")
+    @Test
+    public void isGradeGreaterStudentGradesAssertFalse() {
+        assertFalse(studentGrades.isGradeGreater(89, 92),
+                "failure - should be false");
+    }
+
+    @DisplayName("Check Null for student grades")
+    @Test
+    public void checkNullForStudentGrades() {
+        assertNotNull(studentGrades.checkNull(student.getStudentGrades().getMathGradeResults()),
+                "object should not be null");
+    }
+
+    @DisplayName("Create student without grade init")
+    @Test
+    public void createStudentWithoutGradesInit() {
+        CollegeStudent studentTwo = context.getBean("collegeStudent", CollegeStudent.class);
+        studentTwo.setFirstname("Chad");
+        studentTwo.setLastname("Darby");
+        studentTwo.setEmailAddress("chad.darby@abc.com");
+        assertNotNull(studentTwo.getFirstname());
+        assertNotNull(studentTwo.getLastname());
+        assertNotNull(studentTwo.getEmailAddress());
+        assertNull(studentGrades.checkNull(studentTwo.getStudentGrades()));
+    }
+
+    @DisplayName("Verify students are prototypes")
+    @Test
+    public void verifyStudentsArePrototypes() {
+        CollegeStudent studentTwo = context.getBean("collegeStudent", CollegeStudent.class);
+
+        // prototype bean 은 외부 요청 마다 새롭게 만들어진다.
+        assertNotSame(student, studentTwo);
+
+    }
+
+    @DisplayName("Find Grade Point Average")
+    @Test
+    public void findGradePointAverage() {
+        assertAll("Testing all assertEquals",
+                () -> assertEquals(353.25, studentGrades.addGradeResultsForSingleClass(
+                        student.getStudentGrades().getMathGradeResults())),
+                () -> assertEquals(88.31, studentGrades.findGradePointAverage(
+                        student.getStudentGrades().getMathGradeResults()))
+        );
+    }
+
+
 }
